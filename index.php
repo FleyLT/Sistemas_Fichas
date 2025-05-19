7b854cd2614053b29092f6b3894b637282c4ec25
FleyLT <luaeliseu@gmail.com>
2025-05-19 17:53:00 -0300

    atualização 0.1
    Feito design inicial para login

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Ficha</title>
+    <link rel="stylesheet" type="text/css"   href="estilo.css">
+</head>
+<body>
+    <form action="" method="post">
+        <div class="login-box">
+            <h2>SEJA BEM-VINDO</h2>
+            <input type="text" name="usuario" placeholder="Usuário" />
+            <input type="password" name="senha" placeholder="Senha" />
+            <button type="submit">LOGAR</button>
+            <a href="registro.php" class="register-btn">Registre-se</a>
+        </div>
+    </form>
+
+</body>
+</html>
